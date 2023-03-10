3fed772569e0d741645112d47beeebac5c677a27
Abdullahi Ahmad <beenai@Apples-MBP.lan>
2023-03-10 12:28:57 +0100

    fixed some react error

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,35 +1,35 @@
-// import { Link } from "react-router-dom";
-import styles from "./header.module.scss";
-import { ArrowRight } from "react-bootstrap-icons";
+import { Link } from 'react-router-dom';
+import styles from './header.module.scss';
+import { ArrowRight } from 'react-bootstrap-icons';
 
 const Header = () => {
   return (
     <section className={styles.container}>
       <div className={styles.logo}>Logo</div>
       <ul>
-        <a href="#" className={styles.menu}>
+        <Link to="www.google.com" className={styles.menu}>
           <li>Home</li>
-        </a>
-        <a href="#" className={styles.menu}>
+        </Link>
+        <Link to="www.beenai.com" className={styles.menu}>
           <li>About</li>
-        </a>
-        <a href="#" className={styles.menu}>
+        </Link>
+        <Link to="www.memem.com" className={styles.menu}>
           <li>services</li>
-        </a>
-        <a href="#" className={styles.menu}>
+        </Link>
+        <Link to="www.gtmx.com" className={styles.menu}>
           <li>Contact us</li>
-        </a>
+        </Link>
       </ul>
       <span className={styles.s__menu}>
-        <a href="#" className={styles.menu}>
+        <Link to="www.loginme.com" className={styles.menu}>
           <li>Login</li>
-        </a>
-        <a href="#" className={styles.join__us}>
+        </Link>
+        <Link to="www.hime.com" className={styles.join__us}>
           <li className={styles.j__icons}>
             Join us
             <ArrowRight className={styles.icon__size} />
           </li>
-        </a>
+        </Link>
       </span>
     </section>
   );
